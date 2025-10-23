1feeef1ac924dd28052d396c5f3fc1cf5e58bb0f
sasanamisoushi <k024g0014@g.neec.ac.jp>
2025-10-23 13:50:10 +0900

    イベントの魚の当たり判定修正完了

--- DirectXGame/EventFish.cpp
+++ DirectXGame/EventFish.cpp
@@ -1,39 +1,34 @@
 #include "EventFish.h"
 #include "GameScene.h"
 
-void EventFish::Initialize(Model* model, Camera* camera, Score* Score, const Vector3& pos, bool moveRight) { 
-	
-	   score_ = Score;
-	Fish::Initialize(model, camera, Score, pos, moveRight, 90); 
+void EventFish::Initialize(Model* model, Camera* camera, Score* Score, const Vector3& pos, bool moveRight) {
+
+	score_ = Score;
+	Fish::Initialize(model, camera, Score, pos, moveRight, 90);
 }
 
-void EventFish::Update() { 
-	Fish::Update(); 
+void EventFish::Update() {
+	Fish::Update();
 
-// 行列更新
+	// 行列更新
 	WorldTransformUpdate(GetWorldTransform());
 }
 
 void EventFish::OnCollision(Player* player) {
 	Fish::OnCollision(player);
 
-	if (!hasTriggered_) {
-		hasTriggered_ = true;
-		isLureCheck_ = true;
-		
-		if (isLureCheck_) {
-			// BigFishはスコア加算
-			score_->AddScore(point_);
-		}
+	if (isLureCheck_) {
+		// BigFishはスコア加算
+		score_->AddScore(point_);
+	}
 
-		// イベント発生（群れを出すなど）
-		if (onTriggered_) {
-			onTriggered_(GetWorldPosition(), eventType_);
-		}
+	// イベント発生（群れを出すなど）
+	if (onTriggered_) {
+		onTriggered_(GetWorldPosition(), eventType_);
 	}
 }
 
-AABB EventFish::GetAABB() { 
+AABB EventFish::GetAABB() {
 	Vector3 worldPos = GetWorldPosition();
 
 	AABB aabb;
